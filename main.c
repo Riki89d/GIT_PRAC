1d833377962055a5bcdc535d064d3293019b80f2
Ricard Firmware <ricardcomas@reby.co>
2022-11-07 16:06:30 +0100

    Mitjana feta

--- main.c
+++ main.c
@@ -1,10 +1,19 @@
 #include "guarra.h"
 
+void ferMitjana();
+
 int main(){
 
 printf("Branca neta");
-jsdfjdsf
- dsfdsf
+poso algo aqiu
+
 return 1;
 
+}
+
+void ferMitjana(){
+
+int a,b,m;
+
+m = (a+b)/m;
 }
